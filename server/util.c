a038726ffa5ce866340b580813d84bdaee6497b1
Constantin Wiemer <constantin.wiemer@gmx.de>
2022-09-15 17:59:35 +0200

    Added pack() / unpack() routines

--- server/util.c
+++ server/util.c
@@ -1,17 +1,32 @@
-/*
- * util.h - part of CWDebug, a source-level debugger for the AmigaOS
- *          contains some utility routines
- *
- * Copyright(C) 2018-2022 Constantin Wiemer
- */
+//
+// util.h - part of CWDebug, a source-level debugger for the AmigaOS
+//          This file contains some utility routines.
+//
+// Copyright(C) 2018-2022 Constantin Wiemer
+//
 
 
-#include <proto/exec.h>
+#include <assert.h>
+#ifdef TEST
+    #include <clib/exec_protos.h>
+#else
+    #include <proto/exec.h>
+#endif
 #include <stdarg.h>
 #include <stdio.h>
+#ifdef TEST
+    #include <setjmp.h>
+    #include <cmocka.h>
+#endif
 
 #include "stdint.h"
 #include "util.h"
+
+
+#ifdef TEST
+    #undef assert
+    #define assert(expression) mock_assert((int) (expression), #expression, __FILE__, __LINE__);
+#endif
 
 
 uint8_t g_loglevel;
@@ -31,18 +46,21 @@ void logmsg(const char *p_fname, int lineno, const char *p_func, uint8_t level, 
     va_list args;
     char location[32];
 
-    Forbid();
     if (level < g_loglevel)
         return;
 
     snprintf(location, 32, "%s:%d", p_fname, lineno);
-    printf("0x%08x | %-15s | %-25s | %-5s | ", (uint32_t) FindTask(NULL), location, p_func, p_level_name[level]);
+    // TODO: The correct way would be to mock all system calls instead of just not calling them when we run the tests.
+    #ifdef TEST
+        printf("0x%08x | %-15s | %-25s | %-5s | ", 0xdeadbeef, location, p_func, p_level_name[level]);
+    #else
+        printf("0x%08x | %-15s | %-25s | %-5s | ", (uint32_t) FindTask(NULL), location, p_func, p_level_name[level]);
+    #endif
 
     va_start(args, p_fmtstr);
     vprintf(p_fmtstr, args);
     va_end(args);
     printf("\n");
-    Permit();
 }
 
 
@@ -73,3 +91,285 @@ void dump_memory(const uint8_t *p_addr, uint32_t size)
         pos += 16;
     }
 }
+
+
+int pack_data(uint8_t *p_buffer, uint32_t buf_size, const char *p_format_str, ...)
+{
+    va_list args;
+    const char *p_fmt;
+    uint8_t *p_buf_pos = p_buffer;
+    uint32_t buf_size_left = buf_size;
+    int rc = DOSTRUE;
+
+    // input variables of the respective types, passed in as variadic args
+    uint8_t i8;
+    uint16_t i16;
+    uint32_t i32;
+
+    assert((p_buffer != NULL) && (buf_size > 0) && (p_format_str != NULL));
+    va_start(args, p_format_str);
+    for (p_fmt = p_format_str; *p_fmt != '\0'; p_fmt++) {
+        switch (*p_fmt) {
+            case '!':
+                // We ignore the byte order indicator as we only support network byte order (big endian),
+                // but we want to be compatible with the format strings we use on the host side.
+                break;
+
+            case 'B':
+                if (buf_size_left >= 1) {
+                    i8 = va_arg(args, uint32_t);
+                    *p_buf_pos++ = i8;
+                    --buf_size_left;
+                }
+                else {
+                    LOG(ERROR, "Not enough bytes in buffer to pack byte")
+                    rc = DOSFALSE;
+                    goto exit;
+                }
+                break;
+
+            case 'H':
+                if (buf_size_left >= 2) {
+                    // Variadic args are always extended to an (unsigned) int, so we can't use the actual type in va_arg().
+                    i16 = va_arg(args, uint32_t);
+                    *p_buf_pos++ = (uint8_t) ((i16 >> 8) & 0xff);
+                    *p_buf_pos++ = (uint8_t) (i16 & 0xff);
+                    buf_size_left -= 2;
+                }
+                else {
+                    LOG(ERROR, "Not enough bytes in buffer to pack word")
+                    rc = DOSFALSE;
+                    goto exit;
+                }
+                break;
+
+            case 'I':
+                if (buf_size_left >= 4) {
+                    i32 = va_arg(args, uint32_t);
+                    *p_buf_pos++ = (uint8_t) ((i32 >> 24) & 0xff);
+                    *p_buf_pos++ = (uint8_t) ((i32 >> 16) & 0xff);
+                    *p_buf_pos++ = (uint8_t) ((i32 >> 8) & 0xff);
+                    *p_buf_pos++ = (uint8_t) (i32 & 0xff);
+                    buf_size_left -= 4;
+                }
+                else {
+                    LOG(ERROR, "Not enough bytes in buffer to pack double word")
+                    rc = DOSFALSE;
+                    goto exit;
+                }
+                break;
+
+            default:
+                LOG(ERROR, "Unknown format specifier '%c'", *p_fmt);
+                rc = DOSFALSE;
+                goto exit;
+        }
+    }
+
+    exit:
+        va_end(args);
+        return rc;
+}
+
+
+int unpack_data(uint8_t *p_buffer, uint32_t buf_size, const char *p_format_str, ...)
+{
+    va_list args;
+    const char *p_fmt;
+    uint8_t *p_buf_pos = p_buffer;
+    uint32_t buf_size_left = buf_size;
+    int rc = DOSTRUE;
+
+    // output variables of the respective types, passed in as variadic args
+    uint8_t *p8;
+    uint16_t *p16;
+    uint32_t *p32;
+
+    assert((p_buffer != NULL) && (buf_size > 0) && (p_format_str != NULL));
+    va_start(args, p_format_str);
+    for (p_fmt = p_format_str; *p_fmt != '\0'; p_fmt++) {
+        switch (*p_fmt) {
+            case '!':
+                // We ignore the byte order indicator as we only support network byte order (big endian),
+                // but we want to be compatible with the format strings we use on the host side.
+                break;
+
+            case 'B':
+                if (buf_size_left >= 1) {
+                    p8 = va_arg(args, uint8_t *);
+                    *p8 = *p_buf_pos++;
+                    --buf_size_left;
+                }
+                else {
+                    LOG(ERROR, "Not enough bytes in buffer to unpack byte")
+                    rc = DOSFALSE;
+                    goto exit;
+                }
+                break;
+
+            case 'H':
+                if (buf_size_left >= 2) {
+                    p16 = va_arg(args, uint16_t *);
+                    *p16 = *p_buf_pos++ << 8;
+                    *p16 |= *p_buf_pos++;
+                    buf_size_left -= 2;
+                }
+                else {
+                    LOG(ERROR, "Not enough bytes in buffer to unpack word")
+                    rc = DOSFALSE;
+                    goto exit;
+                }
+                break;
+
+            case 'I':
+                if (buf_size_left >= 4) {
+                    p32 = va_arg(args, uint32_t *);
+                    *p32 = *p_buf_pos++ << 24;
+                    *p32 |= *p_buf_pos++ << 16;
+                    *p32 |= *p_buf_pos++ << 8;
+                    *p32 |= *p_buf_pos++;
+                    buf_size_left -= 4;
+                }
+                else {
+                    LOG(ERROR, "Not enough bytes in buffer to unpack double word")
+                    rc = DOSFALSE;
+                    goto exit;
+                }
+                break;
+
+            default:
+                LOG(ERROR, "Unknown format specifier '%c'", *p_fmt);
+                rc = DOSFALSE;
+                goto exit;
+        }
+    }
+
+    exit:
+        va_end(args);
+        return rc;
+}
+
+
+//
+// unit tests for pack / unpack
+//
+#ifdef TEST
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+static void test_pack_byte(void **state)
+{
+    uint8_t buffer[1];
+    pack_data(buffer, sizeof(buffer), "!B", 0x42);
+    assert_memory_equal(buffer, ((uint8_t[]) {0x42}), 1);
+}
+
+
+static void test_pack_word(void **state)
+{
+    uint8_t buffer[2], expected_buffer[] = {0xca, 0xfe};
+    pack_data(buffer, sizeof(buffer), "!H", 0xcafe);
+    assert_memory_equal(buffer, expected_buffer, 2);
+}
+
+
+static void test_pack_dword(void **state)
+{
+    uint8_t buffer[4], expected_buffer[] = {0xca, 0xfe, 0xba, 0xbe};
+    pack_data(buffer, sizeof(buffer), "!I", 0xcafebabe);
+    assert_memory_equal(buffer, expected_buffer, 4);
+}
+
+
+static void test_pack_wrong_size(void **state)
+{
+    uint8_t buffer[2];
+    int rc = pack_data(buffer, sizeof(buffer), "!I", 0xcafebabe);
+    assert_int_equal(rc, DOSFALSE);
+}
+
+
+static void test_pack_wrong_format(void **state)
+{
+    uint8_t buffer[4];
+    int rc = pack_data(buffer, sizeof(buffer), ">I", 0xcafebabe);
+    assert_int_equal(rc, DOSFALSE);
+}
+
+
+static void test_pack_null_args(void **state)
+{
+    expect_assert_failure(pack_data(NULL, 0, NULL));
+}
+
+
+static void test_unpack_byte(void **state)
+{
+    uint8_t buffer[] = {0x42};
+    uint8_t result;
+    unpack_data(buffer, sizeof(buffer), "!B", &result);
+    assert_int_equal(result, 0x42);
+}
+
+
+static void test_unpack_word(void **state)
+{
+    uint8_t buffer[] = {0xca, 0xfe};
+    uint16_t result;
+    unpack_data(buffer, sizeof(buffer), "!H", &result);
+    assert_int_equal(result, 0xcafe);
+}
+
+
+static void test_unpack_dword(void **state)
+{
+    uint8_t buffer[] = {0xca, 0xfe, 0xba, 0xbe};
+    uint32_t result;
+    unpack_data(buffer, sizeof(buffer), "!I", &result);
+    assert_int_equal(result, 0xcafebabe);
+}
+
+
+static void test_unpack_wrong_size(void **state)
+{
+    uint8_t buffer[] = {0xca, 0xfe};
+    uint16_t result;
+    int rc = unpack_data(buffer, sizeof(buffer), "!I", &result);
+    assert_int_equal(rc, DOSFALSE);
+}
+
+
+static void test_unpack_wrong_format(void **state)
+{
+    uint8_t buffer[] = {0xca, 0xfe};
+    uint16_t result;
+    int rc = unpack_data(buffer, sizeof(buffer), ">H", &result);
+    assert_int_equal(rc, DOSFALSE);
+}
+
+
+static void test_unpack_null_args(void **state)
+{
+    expect_assert_failure(unpack_data(NULL, 0, NULL));
+}
+
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_pack_byte),
+        cmocka_unit_test(test_pack_word),
+        cmocka_unit_test(test_pack_dword),
+        cmocka_unit_test(test_pack_wrong_size),
+        cmocka_unit_test(test_pack_wrong_format),
+        cmocka_unit_test(test_pack_null_args),
+        cmocka_unit_test(test_unpack_byte),
+        cmocka_unit_test(test_unpack_word),
+        cmocka_unit_test(test_unpack_dword),
+        cmocka_unit_test(test_unpack_wrong_size),
+        cmocka_unit_test(test_unpack_wrong_format),
+        cmocka_unit_test(test_unpack_null_args),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
+#pragma GCC diagnostic pop
+#endif
--- server/util.h
+++ server/util.h
@@ -1,28 +1,21 @@
 #ifndef CWDEBUG_UTIL_H
 #define CWDEBUG_UTIL_H
-/*
- * util.h - part of CWDebug, a source-level debugger for the AmigaOS
- *
- * Copyright(C) 2018-2021 Constantin Wiemer
- */
+//
+// util.h - part of CWDebug, a source-level debugger for the AmigaOS
+//
+// Copyright(C) 2018-2022 Constantin Wiemer
+//
 
 
-/*
- * included files
- */
 #include <dos/dos.h>
 #include <exec/types.h>
 
 
-/*
- * external references
- */
-extern UBYTE g_loglevel;
+//
+// constants
+//
 
-
-/*
- * constants / macros
- */
+// log levels
 #define DEBUG 0
 #define INFO  1
 #define WARN  2 
@@ -30,18 +23,26 @@ extern UBYTE g_loglevel;
 #define CRIT  4
 
 
-/*
- * exported functions
- */
+//
+// exported functions
+//
 void logmsg(const char *p_fname, int lineno, const char *p_func, UBYTE level, const char *p_fmtstr, ...);
 void dump_memory(const uint8_t *p_addr, uint32_t size);
+int pack_data(uint8_t *p_buffer, uint32_t buf_size, const char *p_fmt_str, ...);
+int unpack_data(uint8_t *p_buffer, uint32_t buf_size, const char *p_fmt_str, ...);
 
 
-/*
- * macros
- */
+//
+// macros
+//
 #define LOG(level, p_fmtstr, ...) {logmsg(__FILE__, __LINE__, __func__, level, p_fmtstr, ##__VA_ARGS__);}
 #define C_TO_BCPL_PTR(ptr) ((BPTR) (((ULONG) (ptr)) >> 2))
 #define BCPL_TO_C_PTR(ptr) ((APTR) (((ULONG) (ptr)) << 2))
 
-#endif /* CWDEBUG_UTIL_H */
+
+//
+// external references
+//
+extern UBYTE g_loglevel;
+
+#endif // CWDEBUG_UTIL_H
