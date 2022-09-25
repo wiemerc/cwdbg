96e35b590411aa0fb4862698f0017588170e532a
Constantin Wiemer <constantin.wiemer@gmx.de>
2022-09-25 15:00:53 +0200

    Moved the initialization of the debugger object to cwdebug.py completely to prevent circular imports

--- host/cwdebug.py
+++ host/cwdebug.py
@@ -8,15 +8,18 @@
 
 
 import argparse
-import capstone
+import glob
+import os
+import pickle
 import sys
 
 from loguru import logger
 
 from cli import Cli, QuitDebuggerException
 from debugger import dbg
+from errors import ErrorCodes
 from hunklib import get_debug_infos_from_exe
-from server import ServerConnection
+from server import ServerConnection, SrvGetBaseAddress
 from stabslib import ProgramWithDebugInfo
 from ui import MainScreen
 
@@ -43,7 +46,7 @@ def main():
                     logger.debug("Exiting debugger...")
                     break
         else:
-            main_screen = MainScreen(args.verbose)
+            MainScreen(args.verbose)
     except Exception as e:
         logger.exception(f"Internal error occurred: {e}")
     finally:
@@ -89,14 +92,37 @@ def _print_banner():
     """)
 
 
-# TODO: Move routine to debugger object once the circular import debugger.py <-> cli.py is broken
 def _init_debugger(args: argparse.Namespace):
-        if args.prog:
-            dbg.program = ProgramWithDebugInfo.from_stabs_data(get_debug_infos_from_exe(args.prog))
-        dbg.server_conn = ServerConnection(args.host, args.port) 
-        dbg.cli = Cli()
-        dbg.load_syscall_db(args.syscall_db_dir)
-        dbg.get_lib_base_addresses(args.syscall_db_dir)
+    if args.prog:
+        dbg.program = ProgramWithDebugInfo.from_stabs_data(get_debug_infos_from_exe(args.prog))
+    dbg.server_conn = ServerConnection(args.host, args.port) 
+    dbg.cli = Cli()
+    dbg.syscall_db = _load_syscall_db(args.syscall_db_dir)
+    dbg.lib_base_addresses = _get_lib_base_addresses(args.syscall_db_dir)
+
+
+def _load_syscall_db(syscall_db_dir: str):
+    logger.info("Loading system call database")
+    syscall_db = {}
+    for fname in glob.glob(os.path.join(syscall_db_dir, '*.data')):
+        with open(fname, 'rb') as f:
+            syscall_db[os.path.splitext(os.path.basename(fname))[0]] = pickle.load(f)
+    return syscall_db
+
+
+def _get_lib_base_addresses(syscall_db_dir: str):
+    logger.info("Getting library base addresses from server")
+    lib_base_addresses = {}
+    file_names = glob.glob(os.path.join(syscall_db_dir, '*.data'))
+    lib_names = [os.path.splitext(os.path.basename(fname))[0] for fname in file_names]
+    for lname in lib_names:
+        cmd = SrvGetBaseAddress(library_name=lname + '.library').execute(dbg.server_conn)
+        if cmd.error_code == ErrorCodes.ERROR_OK:
+            logger.debug(f"Library '{lname}.library' has address {hex(cmd.result)}")
+            lib_base_addresses[cmd.result] = lname
+        else:
+            raise RuntimeError(f"Getting library base addresses failed with error code {ErrorCodes(cmd.error_code).name}")
+    return lib_base_addresses
 
 
 if __name__ == '__main__':
--- host/debugger.py
+++ host/debugger.py
@@ -5,7 +5,7 @@
 # Copyright(C) 2018-2022 Constantin Wiemer
 
 
-import capstone
+import argparse
 import glob
 import os
 import pickle
@@ -13,44 +13,18 @@ import pickle
 from dataclasses import dataclass
 from typing import Optional
 
-from loguru import logger
 
-from errors import ErrorCodes
-from server import ServerConnection, SrvGetBaseAddress
-from stabslib import ProgramWithDebugInfo
-from target import SyscallInfo, TargetInfo
-
-
+# This is the global debugger object used to pass around all other objects that are needed by the other modules. It
+# will be populated in cwdebug.py. We can't do this here because this would require us to import several things from
+# other modules, and thus would cause circular imports. It's basically a form of dependency injection.
 @dataclass
 class Debugger:
-    program: ProgramWithDebugInfo | None = None
-    server_conn: ServerConnection | None  = None
-    # TODO: We can reference Cli only by name because cli.py also imports us -> would cause circular import. But
-    #       maybe we don't need to store the CLI object here?
+    program: Optional['ProgramWithDebugInfo'] = None
+    server_conn: Optional['ServerConnection'] = None
     cli: Optional['Cli'] = None
-    syscall_db: dict[str, dict[int, SyscallInfo]] | None = None
+    syscall_db: dict[str, dict[int, 'SyscallInfo']] | None = None
     lib_base_addresses: dict[int, str] | None = None
-    target_info: TargetInfo | None = None
-
-    def load_syscall_db(self, syscall_db_dir: str):
-        logger.info("Loading system call database")
-        self.syscall_db = {}
-        for fname in glob.glob(os.path.join(syscall_db_dir, '*.data')):
-            with open(fname, 'rb') as f:
-                self.syscall_db[os.path.splitext(os.path.basename(fname))[0]] = pickle.load(f)
-
-    def get_lib_base_addresses(self, syscall_db_dir: str):
-        logger.info("Getting library base addresses from server")
-        self.lib_base_addresses = {}
-        file_names = glob.glob(os.path.join(syscall_db_dir, '*.data'))
-        lib_names = [os.path.splitext(os.path.basename(fname))[0] for fname in file_names]
-        for lname in lib_names:
-            cmd = SrvGetBaseAddress(library_name=lname + '.library').execute(self.server_conn)
-            if cmd.error_code == ErrorCodes.ERROR_OK:
-                logger.debug(f"Library '{lname}.library' has address {hex(cmd.result)}")
-                self.lib_base_addresses[cmd.result] = lname
-            else:
-                raise RuntimeError(f"Getting library base addresses failed with error code {ErrorCodes(cmd.error_code).name}")
+    target_info: Optional['TargetInfo'] = None
 
 
-dbg = Debugger()
+dbg =  Debugger()
