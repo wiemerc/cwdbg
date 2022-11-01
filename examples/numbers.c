8a08279d4f8f3190fa8fd53678ce26b57b0d65f9
Constantin Wiemer <constantin.wiemer@gmx.de>
2022-11-01 19:12:30 +0100

    Rewrote example so that main() calls another function (for testing the 'backtrace' command)

--- examples/numbers.c
+++ examples/numbers.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_TRAIT_SIZE 100
+
+static void get_num_trait(int i, char *num_trait_buffer, int buffer_size)
+{
+    if (i % 3 == 0)
+        strncpy(num_trait_buffer, "divisible by 3", buffer_size - 1);
+    else if (i % 2 == 0)
+        strncpy(num_trait_buffer, "even", buffer_size - 1);
+    else
+        strncpy(num_trait_buffer, "odd", buffer_size - 1);
+    num_trait_buffer[buffer_size - 1] = 0;
+}
 
 int main()
 {
     int i;
-    for (i = 1; i <= 10; i++) {
-        printf("%d", i);
-        if ((i % 2) == 0)
-            printf(" is even\n");
-        else if ((i % 3) == 0)
-            printf(" is dividable by 3\n");
-        else
-            printf("\n");
+    char num_trait[MAX_TRAIT_SIZE];
+
+    for (i = 0; i < 10; i++) {
+        get_num_trait(i, num_trait, MAX_TRAIT_SIZE);
+        printf("%d is %s\n", i, num_trait);
     }
     return 0;
 }
